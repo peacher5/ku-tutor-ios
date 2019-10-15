e405386989a529ce68f375f5fc5298489eb6c0c2
Peranut W <peacher5@hotmail.com>
2019-10-15 14:37:35 +0700

    Initial Commit

---
+++ tutor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutor
+//
+//  Created by Peranut W. on 15/10/2562 BE.
+//  Copyright © 2562 KU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
